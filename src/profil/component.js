550a8fa61cbdc18aeae81ecc97877919e63bd9cd
yaa55 <ammaryacine55@gmail.com>
2021-04-16 17:29:48 +0100

    first commit

---
+++ src/profil/component.js
@@ -0,0 +1,21 @@
+const App = () => {
+            <div>
+              < sarah name = 'sarah'
+              lastname = 'smith'
+              age = '20'
+              city = 'tunis'
+              profession = 'institurice'
+               /> 
+                 
+     
+        
+           
+            </div>
+             function handleName(props) {
+              return name; }
+      
+      handleName.propstypes = {
+      }
+
+               }
+     
